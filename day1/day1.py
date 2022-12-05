510a2dd9f33cb3a7b502e8bf0378baa40781f81f
Friberg <JFRIBER4@volvocars.com>
2022-12-05 13:19:44 +0100

    day1 refactor

--- day1/day1.py
+++ day1/day1.py
@@ -1,45 +1,31 @@
 
 
-def first_task(lines: list) -> int:
-    current = 0
-    max = 0
-
-    for line in lines:
-        if len(line) > 1:
-            current = current + int(line)
-        else:
-            if current > max:
-                max = current
-            current = 0
-    return max
+def main() -> None:
+    with open('day1\calories.txt') as f:
+        lines = f.readlines()
+    print(f"Part 1: {max(sum_elfs(lines))}")
+    print(f"Part 2: {sum_three_max(sum_elfs(lines))}")
 
 
-def second_task(lines: list) -> int:
-    current = 0
+def sum_elfs(lines: list) -> list:
     elfs = []
-    max_total = 0
-
+    current = 0
     for i in range(len(lines)):
         if len(lines[i]) > 1 or lines[i].find("\n") == -1:
             current = current + int(lines[i])
         if i == len(lines) or len(lines[i]) == 1:
             elfs.append(current)
             current = 0
-    
+    return elfs
+
+
+def sum_three_max(elfs: list) -> int:
+    max_total = 0
     for _ in range(3):
         max_total = max_total + max(elfs)
         elfs.pop(elfs.index(max(elfs)))
-
     return max_total
 
 
 if __name__ == "__main__":
-
-    with open('day1\calories.txt') as f:
-        lines = f.readlines()
-
-    # with open('day1\\test.txt') as f:
-    #     lines = f.readlines()
-
-    print(first_task(lines))
-    print(second_task(lines))
+    main()
